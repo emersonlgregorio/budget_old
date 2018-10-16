8f8a5556b3a858609431e3be7eca3ec5bcb37ffd
Emerson Gregorio <emersonlgregorio@gmail.com>
2018-10-15 23:53:59 -0400

    commit 5

--- budget/settings.py
+++ budget/settings.py
@@ -57,8 +57,8 @@ INSTALLED_APPS = [
     'core',
     'empresa',
     #apps de terceiros
-    'debug_panel',
-    'debug_toolbar',
+    #'debug_panel',
+    #'debug_toolbar',
     'widget_tweaks',
 ]
 
